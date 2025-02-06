30f9d0c03a267abe204bfe736a68e9bba8da2c17
Verdysh Volodymyr <verdyshvladimir@gmail.com>
2025-02-06 17:43:33 +0200

    fixed bugs

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,27 +8,27 @@
  */
 function transformStateWithClones(state, actions) {
   const newState = [];
-  let destruction = { ...state };
+  let stateCopy = { ...state };
 
   for (let i = 0; i < actions.length; i++) {
-    const clonedState = { ...destruction };
+    const clonedState = { ...stateCopy };
 
-    if (actions[i].type === 'addProperties') {
-      Object.assign(clonedState, actions[i].extraData);
+    switch (actions[i].type) {
+      case 'addProperties':
+        Object.assign(clonedState, actions[i].extraData);
+        break;
+      case 'removeProperties':
+        for (let key = 0; key < actions[i].keysToRemove.length; key++) {
+          delete clonedState[actions[i].keysToRemove[key]];
+        }
+        break;
+      case 'clear':
+        for (const key in clonedState) {
+          delete clonedState[key];
+        }
+        break;
     }
-
-    if (actions[i].type === 'removeProperties') {
-      for (let key = 0; key < actions[i].keysToRemove.length; key++) {
-        delete clonedState[actions[i].keysToRemove[key]];
-      }
-    }
-
-    if (actions[i].type === 'clear') {
-      for (const key in clonedState) {
-        delete clonedState[key];
-      }
-    }
-    destruction = clonedState;
+    stateCopy = clonedState;
     newState.push(clonedState);
   }
 
